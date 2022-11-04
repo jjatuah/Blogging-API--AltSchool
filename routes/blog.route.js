a4f67b88e876e326e6e8d81cd10ae8fe5824fb61
unknown <jjatuah@gmail.com>
2022-11-04 23:59:25 +0100

    updated get request for author blog route

--- routes/blog.route.js
+++ routes/blog.route.js
@@ -53,42 +53,21 @@ blogRoute.post('/', async (req, res) => {
 
 blogRoute.get('/', async (req, res) => {
 
-  const {author, title, state, tags, read_count, reading_time, time, page = 1, limit = 20} = req.query;
+  const {secret_token, page = 1, limit = 20} = req.query;
 
-  let readCountNum;
-  let readTimeNum;
-  let timeNum;
+  const state = req.body.state
 
-  read_count === "asc" ? readCountNum = 1 : readCountNum = -1;
-  reading_time === "asc" ? readTimeNum = 1 : readTimeNum = -1;
-  time === "asc" ? timeNum = 1 : timeNum = -1;
+  const jwtDecoded = jwt.decode(secret_token);
+
+  const blogAuthor = jwtDecoded.user.fullname;
+  
 
   try {
     let blogs;
-    if (author) {
-      blogs = await blogModel.find({ author }).limit(limit * 1).skip((page - 1) * limit);
-    } else if (title) {
-      blogs = await blogModel.find({ title }).limit(limit * 1).skip((page - 1) * limit);
-    }else if (state) {
-      blogs = await blogModel.find({ state }).limit(limit * 1).skip((page - 1) * limit);
-    } else if (tags) {
-      blogs = await blogModel.find({ tags: {
-        $in: [tags]
-      } }).limit(limit * 1).skip((page - 1) * limit);
-    } else if (read_count) {
-      blogs = await blogModel.find({}).sort({
-        read_count : readCountNum
-      }).limit(limit * 1).skip((page - 1) * limit);
-    } else if (reading_time) {
-      blogs = await blogModel.find({}).sort({
-        reading_time : readTimeNum
-      }).limit(limit * 1).skip((page - 1) * limit);
-    } else if (time) {
-      blogs = await blogModel.find({}).sort({
-        timestamps : timeNum
-      }).limit(limit * 1).skip((page - 1) * limit);
+    if (state) {
+      blogs = await blogModel.find({ author: blogAuthor, state }).limit(limit * 1).skip((page - 1) * limit);
     } else {
-      blogs = await blogModel.find().limit(limit * 1).skip((page - 1) * limit);
+      blogs = await blogModel.find({ author: blogAuthor }).limit(limit * 1).skip((page - 1) * limit);
     }
     res.status(200).json({total_blogs: blogs.length, blogs});
   } catch (err) {
@@ -98,26 +77,6 @@ blogRoute.get('/', async (req, res) => {
 })
 
 
-blogRoute.get('/:blogId', async (req, res) => {
-    const { blogId } = req.params;
-    
-    await blogModel.findById(blogId)
-        .then((blog)=> {
-            if(!blog) {
-                return res.status(404).json({ status: false, blog : null })
-            }
-
-            blog.read_count++
-
-            blog.save();
-
-            const { author, ...result } = blog;
-            const blogResult = result._doc
-            return res.json({ status: true, witten_by : author, blogResult }) 
-        }).catch((err) => {
-            return res.json({ status: false, message: err })
-    }) 
-})
 
 blogRoute.patch('/:id', async (req, res) => {
 
